edd5202bf34f924513e1e574b6549036011a7b77
Chris-253 <christophershoem@u.boisestate.edu>
2026-03-30 11:32:38 -0600

    Added to do and example comments

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 FILE_NAME=test.bin
 
+# Example how to open a file and print content 
+#
+# f = open("demofile.txt", "r")
+# print(f.read())
+
+# "r" - Read - Default value. Opens a file for reading
+# "a" - Append - Opens a file for appending, creates if doesn't exist
+# "w" - Write - Opens a file for writing, creates file if doesn't exist
+# "x" - Create - Creates the specified file, returns an error if file exist
+
+# TO-DO:
+# - Take string input (Use array then file)
+# - Split the string into sections
+# - Convert string to int
+# - Convert into to bin
+# - Write bin to file
+
 def to_bin(var):
     print("Write var to disk in binary format")
+    f = open("demofile.txt", "a")
+    print(f.read())
 
 def from_bin(file):
     print("Reading binary file to mem")
